a2636386041bdb68646b298bd2047c92aca016d6
Nikolaj Schlej <nikolaj.schlej@ymail.com>
2012-10-07 21:08:38 +0200

    Version 0.5.2
    - Functions renamed to prevent compilation errors on MacOS X and Linux

---
+++ FD44Copier/FD44Copier/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  FD44Copier
+//
+//  Created by Nikolaj Schlej on 10/7/12.
+//  Copyright (c) 2012 Nikolaj Schlej. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
